c9084f644afefb2ec653a8d0ec70245e9cd42a49
Meenakshi Sivapriya <meenakshisivapriya@gmail.com>
2017-03-31 19:07:31 +0530

    Unity Files
    Adding Files

---
+++ Assets/Scripts/Resizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resizer : MonoBehaviour {
+
+	public GameObject model;
+	public Scaler scaler;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void zoomin()
+	{
+		scaler.inc ();
+	}
+
+	public void zoomout()
+	{
+		scaler.dec ();
+	}
+}
---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	private float load;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		load += Time.deltaTime;
+		if (load > 1)
+			Application.LoadLevel ("Main");
+	}
+}
